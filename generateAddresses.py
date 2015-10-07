6d8e159959b9deef0df6615af97b5bf8371ee0f9
STumbles <imstumbles@gmail.com>
2015-10-06 19:57:42 -0700

    added multithreading

--- generateAddresses.py
+++ generateAddresses.py
@@ -1,5 +1,6 @@
 import utils
 import argparse
+import threading
 from random import randrange
 from sys import exit
 
@@ -8,13 +9,21 @@ parser.add_argument("-n", type=int, help="number of addresses to generate.")
 args = parser.parse_args()
 
 def main():
-	if args.n:
-		fp = open("generatedAddresses", 'a+')
-		for i in xrange(args.n):
-			private_key = ''.join(['%x' % randrange(16) for x in range(0, 64)])
-			wif = utils.base58CheckEncode(0x80, private_key.decode('hex'))
-			address = utils.pubKeyToAddr(utils.privateKeyToPublicKey(utils.wifToPrivateKey(wif)))
-			fp.write("PK: " + private_key + '\n' + "WIF: " +wif + '\n' + "ADDR: " +address+'\n')
+	counter = 0
+	try:
+		if args.n:
+			fp = open("ga1", 'a+')
+			for i in xrange(args.n):
+				print counter
+				private_key = ''.join(['%x' % randrange(16) for x in range(0, 64)])
+				wif = utils.base58CheckEncode(0x80, private_key.decode('hex'))
+				address = utils.pubKeyToAddr(utils.privateKeyToPublicKey(utils.wifToPrivateKey(wif)))
+				fp.write("PK: " + private_key + '\n' + "WIF: " +wif + '\n' + "ADDR: " +address+'\n')
+				counter = counter + 1
+			fp.close()
+			print "File successfully closed."
+	except KeyboardInterrupt:
+		print "[!] User Keyboard Interrupt"
 		fp.close()
 		print "File successfully closed."
 
@@ -22,4 +31,8 @@ def main():
 	exit()
 
 if __name__ == "__main__":
-	main()
+	threads = []
+	for i in range(4):
+	    t = threading.Thread(target=main, args=())
+	    threads.append(t)
+	    t.start()
